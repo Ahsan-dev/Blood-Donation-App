7821eb5637067eb6070b7e58e3b6def29da9bdd5
Ahsan-dev <46849763+Ahsan-dev@users.noreply.github.com>
2021-08-14 23:17:48 +0600

    This is a blood donation system app

--- app/src/main/java/com/example/bloodbank/Adapter/HistoryRecyclerAdapter.java
+++ app/src/main/java/com/example/bloodbank/Adapter/HistoryRecyclerAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.bloodbank.Models.HistoryModel;
+import com.example.bloodbank.Permanent;
 import com.example.bloodbank.R;
 import com.example.bloodbank.ViewHolder.HistoryViewHolder;
 
@@ -13,6 +14,7 @@ import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import io.paperdb.Paper;
 
 public class HistoryRecyclerAdapter extends RecyclerView.Adapter<HistoryViewHolder> {
 
@@ -38,13 +40,32 @@ public class HistoryRecyclerAdapter extends RecyclerView.Adapter<HistoryViewHold
 
         HistoryModel hisModel = hisList.get(position);
 
-        holder.histYearTxt.setText(hisModel.getYear());
-        holder.histMonthTxt.setText(hisModel.getMonth());
-        holder.histDateTxt.setText(hisModel.getDate());
-        holder.histLocationTxt.setText(hisModel.getLocation());
+        holder.histYearTxt.setText(hisModel.getUpdatedAt().substring(0,3));
+
+        int month = Integer.parseInt(hisModel.getUpdatedAt().substring(5,6));
+        String mon = "";
+
+                if(month==1) mon = "January";
+                else if(month==2) mon = "February";
+                else if(month==3) mon = "March";
+                else if(month==4) mon = "Apeil";
+                else if(month==5) mon = "May";
+                else if(month==6) mon = "June";
+                else if(month==7) mon = "July";
+                else if(month==8) mon = "August";
+                else if(month==9) mon = "September";
+                else if(month==10) mon = "Otcober";
+                else if(month==11) mon = "November";
+                else if(month==12) mon = "December";
+
+        holder.histMonthTxt.setText(mon);
+        holder.histDateTxt.setText(hisModel.getUpdatedAt().substring(7,8));
+
+        String loc = hisModel.getPoliceStation()+", "+hisModel.getDistrict();
+        holder.histLocationTxt.setText(loc);
         holder.histHospitalTxt.setText(hisModel.getHospital());
         holder.histBloodGrpTxt.setText(hisModel.getBloodGrp());
-        holder.histDaysAgoTxt.setText(hisModel.getDaysAgo()+" Days Ago");
+        holder.histDaysAgoTxt.setText(" "+" Days Ago");
 
     }
 
